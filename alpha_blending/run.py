b1c41e52cb90c611c6638bd4f869a4d48b7bfd31
Ayush Ganvir <ayushganvir143@gmail.com>
2022-08-03 20:47:39 +0530

    Alpha Blending

--- alpha_blending/run.py
+++ alpha_blending/run.py
@@ -1,24 +1,30 @@
 import cv2
 
-foreground = cv2.imread("puppets.png")
-background = cv2.imread("ocean.png")
-alpha = cv2.imread("puppets_alpha.png")
+input = 'input.jpg'
+red = 'red.png'
+mask = 'mask.png'
+alpha_scale = 2.24
+background_y_scale = 33.6
+background_x_scale = 22.4
+foreground = cv2.imread(input)
+background = cv2.imread(red)
+alpha = cv2.imread(mask)
+alpha = cv2.resize(alpha, (0, 0), fx = alpha_scale, fy = alpha_scale)
+background = cv2.resize(background, (0, 0), fx = background_x_scale, fy = background_y_scale)
 
- 
-# Convert uint8 to float
+
 foreground = foreground.astype(float)
 background = background.astype(float)
 
 alpha = alpha.astype(float)/255
 
-# Multiply the foreground with the alpha matte
-
 foreground = cv2.multiply(alpha, foreground)
-# Multiply the background with ( 1 - alpha )
 background = cv2.multiply(1.0 - alpha, background)
-# Add the masked foreground and background.
 outImage = cv2.add(foreground, background)
- 
-# Display image
+outImage = cv2.resize(outImage, (0, 0), fx = 0.05, fy = 0.05)
+
+cv2.imwrite("alpha_blending/result.jpg", outImage)
 cv2.imshow("result", outImage/255)
+# cv2.destroyWindow('result')
+
 cv2.waitKey(0)
